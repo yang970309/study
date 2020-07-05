8fa0c87b02bf65c35208d0c0fca2614a0c5878bd
yang <18538744813@163.com>
2020-07-05 13:18:23 +0800

    idea 第二次 推送

--- test/src/cn/yang/test/test.java
+++ test/src/cn/yang/test/test.java
@@ -6,5 +6,6 @@ public class test {
         System.out.println("hello devlop");
         System.out.println("测试idea git");
         System.out.println("idea 推送的");
+        System.out.println("在试一遍");
     }
 }
