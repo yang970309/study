3c39696fc20a2c4742dc9d756a1d6fc72d4bd202
yang <18538744813@163.com>
2020-07-05 13:06:32 +0800

    测试IdeaGit

--- test/src/cn/yang/test/test.java
+++ test/src/cn/yang/test/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello devlop");
+        System.out.println("测试idea git");
     }
 }
